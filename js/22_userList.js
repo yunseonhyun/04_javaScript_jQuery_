90e0c16682728101213558d5b0e6b2d616e37875
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-08-04 12:03:31 +0900

    localStorage .get .set .parse .stringify()

--- js/22_userList.js
+++ js/22_userList.js
@@ -1,40 +1,28 @@
-$(function() {
-    showUsers();
-})
+$(function () {
+  showUsers();
+});
 
 function showUsers() {
-    // localStorage에서 모든 데이터를 담을 수 있는 리스트 변수이름 생성
+  // localStorage에서 모든 데이터를 담을 수 있는 리스트 변수이름 생성
 
-    const users = []; // localStorage에 저장된 정보를 목록 형태로 users 내부에 담아둘 것
+  // 기존 회원 목록 가져오기 (없으면 빈 배열 형태) 가져온 값을 userList 변수 이름에 담아두기
+  let userList = JSON.parse(localStorage.getItem("userList") || "[]");
 
-    // username, userpw가 있으면 배열에 추가
+  // 사용자 총 회원수표시 users.length
 
-    const username = localStorage.getItem('username');
-    const userpw = localStorage.getItem('userpw');
+  // 사용자가 없으면 users.length === 0 no~users 볼 수 있음!
 
-    if (username && userpw) {
-        username.push({
-            id: username,
-            pw: userpw,
-        });
+  // map 사용해서 HTML로 소비자가 유저 리스트 목록을 확인할 수 있도록 설정
 
-    }
-
-    // 사용자 총 회원수표시 users.length
-
-    // 사용자가 없으면 users.length === 0 no~users 볼 수 있음!
-
-    // map 사용해서 HTML로 소비자가 유저 리스트 목록을 확인할 수 있도록 설정
-
-    const userHTML = users.map(
-        u =>
-            `
+  const userHTML = userList.map(
+    (u) =>
+      `
         <div class="user-item">
-            <div class="user-id">아이디 : ${u.id}</div>
-            <div class="user-pw">비밀번호 : ${u.pw}</div>
+            <div class="user-id">아이디 : ${u.username}</div>
+            <div class="user-pw">비밀번호 : ${u.password}</div>
         </div>
         `
-    );
+  );
 
-    $("#user-list").html(userHTML);
+  $("#user-list").html(userHTML);
 }
